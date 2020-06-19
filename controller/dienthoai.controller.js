4b3db3c016c0d8e70212d9fe47ebb6acbad349e8
=prowa789 <=asacnuoc@gmail.com>
2020-06-20 01:58:54 +0700

    make it better

--- controller/dienthoai.controller.js
+++ controller/dienthoai.controller.js
@@ -42,7 +42,7 @@ module.exports.comment_post = async function (req, res) {
             noi_dung: req.body.noi_dung,
             email: req.body.email,
             sdt: req.body.sdt,
-            ngay_gio: new Date()
+            ngay_gio: Date.now()
         });
         binhluan.save();
         res.redirect('/dienthoai/' + phone_id);
